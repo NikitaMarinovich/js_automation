70783e9c5d34dece6dee10421568363a5a4683e4
Nikita <qa.marinovich@gmail.com>
2025-09-27 00:06:55 +0300

    Day 1-2 : Variables and Data types

---
+++ week1-js/Day_2/datatypes.js
@@ -0,0 +1,23 @@
+// let randomNum = Math.random()         // generates 0 to 0.999
+// let numBtnZeroAndTen = randomNum * 11
+
+// console.log(numBtnZeroAndTen)         // this gives: min 0 and max 10.99
+
+// let randomNumRoundToFloor = Math.floor(numBtnZeroAndTen)
+// console.log(randomNumRoundToFloor)    // this gives between 0 and 10
+
+
+ let num1 = "1234"
+ let num2 = "605"
+ let num3 = "1405"
+ let num4 = "-7"
+
+let numInt1 = Number(num1)
+let numInt2 = Number(num2)
+let numInt3 = Number(num3)
+let numInt4 = Number(num4)
+// let numInt = parseInt(num1)
+console.log(numInt1)
+console.log(numInt2)
+console.log(numInt3)
+console.log(numInt4)

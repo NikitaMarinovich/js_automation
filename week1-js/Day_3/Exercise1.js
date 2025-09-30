f179cac5545dca33f489777db94f2ddf73700868
Nikita <qa.marinovich@gmail.com>
2025-09-30 20:28:05 +0300

    Day 3

---
+++ week1-js/Day_3/Exercise1.js
@@ -0,0 +1,15 @@
+// Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types
+
+const firstName = 'Nik'
+let lastName = 'Marinovich'
+let country = 'BY'
+let city = 'Minsk'
+let age = 23
+let isMarried = true
+
+console.log(typeof firstName)
+console.log(typeof lastName)
+console.log(typeof country)
+console.log(typeof city)
+console.log(typeof age)
+console.log(typeof isMarried)

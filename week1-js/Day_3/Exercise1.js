c8347d429335814b003bfc0bfdb1dda231b2180f
Nikita <qa.marinovich@gmail.com>
2025-10-02 23:13:13 +0300

    Day 3 Exercise 1

--- week1-js/Day_3/Exercise1.js
+++ week1-js/Day_3/Exercise1.js
@@ -1,15 +1,118 @@
-// Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types
+// 1 Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types
 
-const firstName = 'Nik'
-let lastName = 'Marinovich'
-let country = 'BY'
-let city = 'Minsk'
-let age = 23
-let isMarried = true
+// const firstName = 'Nik'
+// let lastName = 'Marinovich'
+// let country = 'BY'
+// let city = 'Minsk'
+// let age = 23
+// let isMarried = true
 
-console.log(typeof firstName)
-console.log(typeof lastName)
-console.log(typeof country)
-console.log(typeof city)
-console.log(typeof age)
-console.log(typeof isMarried)
+// console.log(typeof firstName)
+// console.log(typeof lastName)
+// console.log(typeof country)
+// console.log(typeof city)
+// console.log(typeof age)
+// console.log(typeof isMarried)
+
+
+
+
+// 2 Check if type of '10' is equal to 10
+
+// console.log('10' == 10)
+
+
+
+
+// 3 Check if parseInt('9.8') is equal to 10
+
+// console.log(parseInt('9.8') == 10)
+
+
+
+
+// 4 Boolean value is either true or false.
+
+// Write three JavaScript statement which provide truthy value.
+// Write three JavaScript statement which provide falsy value.
+
+// console.log(1 == 1)
+// console.log(1 === 1)
+// console.log(1 >= 1)
+
+// console.log(1 == 11)
+// console.log(1 !== 1)
+// console.log(1 >= 11)
+
+
+
+// 5 Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+
+// 4 > 3 true
+// 4 >= 3 true
+// 4 < 3 false
+// 4 <= 3 false 
+// 4 == 4 true 
+// 4 === 4 true
+// 4 != 4 false
+// 4 !== 4 false
+// 4 != '4' false
+// 4 == '4' true
+// 4 === '4' false
+
+// Find the length of python and jargon and make a falsy comparison statement.
+
+// let lang = 'python and jargon'
+// console.log(lang.length >= 20)
+
+
+
+// 6 Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log()
+
+// 4 > 3 && 10 < 12 true 
+// 4 > 3 && 10 > 12 false
+// 4 > 3 || 10 < 12 true
+// 4 > 3 || 10 > 12 true
+// !(4 > 3) false
+// !(4 < 3)  true
+// !(false) true
+// !(4 > 3 && 10 < 12) false
+// !(4 > 3 && 10 > 12) true
+// !(4 === '4') false
+// There is no 'on' in both dragon and python
+
+// let lang = 'dragon and python'
+// let pattern = /on/gi
+// console.log(lang.match(pattern).length == false)
+// console.log(lang.match('on'))
+
+
+
+
+// 7  Use the Date object to do the following activities
+
+// What is the year today?
+// const now = new Date()
+// console.log(now.getFullYear())
+
+// What is the month today as a number?
+// const now = new Date()
+// console.log(now.getMonth())
+// What is the date today?
+// const now = new Date()
+// console.log(now.getDate())
+// What is the day today as a number?
+// const now = new Date()
+// console.log(now.getDay())
+
+// What is the hours now?
+// const now = new Date()
+// console.log(now.getHours())
+
+// What is the minutes now?
+// const now = new Date()
+// console.log(now.getMinutes())
+
+// Find out the numbers of seconds elapsed from January 1, 1970 to now.
+// const now = new Date()
+// console.log(now.getTime())

8ed2f5873d5489b5f5f85f3bec1256004a0809e3
Nikita <qa.marinovich@gmail.com>
2025-10-05 22:04:17 +0300

    Day 3 Exercise 2

---
+++ week1-js/Day_3/Exercise2.js
@@ -0,0 +1,25 @@
+// Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+
+
+// let base = prompt('Enter Base')
+// let height = prompt('Enter Height')
+// console.log(base)
+// console.log(height)
+// let area = 0.5 * base * height
+// console.log(`The area of the triangle is ${area}`)
+
+// Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
+
+// let sideA = prompt('Enter side A')
+// let sideB = prompt('Enter side B')
+// let sideC = prompt('Enter side C')
+// console.log(sideA, sideB, sideC)
+// let numbA = Number(sideA)
+// let numbB = Number(sideB)
+// let numbC = Number(sideC)
+
+// let perimeter = (numbA + numbB + numbC)
+// console.log(`The area of the triangle is ${perimeter}`)
+
+// Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+

131c6ed332db351cf25bf168bf87ee72b0516426
Nikita <qa.marinovich@gmail.com>
2025-10-06 23:32:07 +0300

    Exercise 2

--- week1-js/Day_3/Exercise2.js
+++ week1-js/Day_3/Exercise2.js
@@ -1,4 +1,4 @@
-// Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+// 1. Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
 
 
 // let base = prompt('Enter Base')
@@ -8,7 +8,7 @@
 // let area = 0.5 * base * height
 // console.log(`The area of the triangle is ${area}`)
 
-// Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
+// 2. Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
 
 // let sideA = prompt('Enter side A')
 // let sideB = prompt('Enter side B')
@@ -21,5 +21,27 @@
 // let perimeter = (numbA + numbB + numbC)
 // console.log(`The area of the triangle is ${perimeter}`)
 
-// Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+// 3.  Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+ 
+// let length = prompt('Lets calculate area and Perimetr', )
+// let width = prompt('Enter width')
+// let numLength = Number(length)
+// let numWidth = Number(width)
+// let area = numLength * numWidth
+// console.log(area)
 
+
+// let perimeter = 2 * (numLength + numLength)
+// console.log(perimeter)
+
+
+// 4.  Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+
+// const pi = Math.PI
+// let radius = prompt('Enter circle radius')
+// const numRadius = Number(radius)
+// let area = pi * Math.pow(numRadius,2);
+// console.log(area)
+
+// let circumference = 2 * pi * numRadius
+// console.log(circumference)
